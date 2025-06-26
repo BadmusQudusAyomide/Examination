31473cb2b3a317f6639d8fb6bd5e4c0277656d67
Badmus Qudus Ayomide <badmusqudusayomide@gmail.com>
2025-06-26 07:56:40 +0100

    doone

--- conn.php
+++ conn.php
@@ -1,16 +1,17 @@
-<?php 
+<?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "cee_db";
+$host = "193.180.215.213";
+$port = "64321";
+$user = "user_77c31690";
+$pass = "5e0e82c123bce1465227a54d5db0e8f3";
+$db = "db_31f6ac6a";
 $conn = null;
 
 try {
-  $conn = new PDO("mysql:host={$host};dbname={$db};",$user,$pass);
+  $conn = new PDO("mysql:host={$host};port={$port};dbname={$db};", $user, $pass);
 } catch (Exception $e) {
-  
+
 }
 
 
- ?>
+?>
